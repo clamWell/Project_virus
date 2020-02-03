fd4da6ba3f60eca7ca065a2eb3d029787198c1d1
h62446244! <pepesi258@hanmail.net>
2020-02-03 17:04:05 +0900

    [21세기바이러스] * 하단에 전염병의 역사 내용 추가 * 내부 텍스트 데스킹 본 텍스트로 변경 * 범례에 bgColor 추가

---
+++ dataload.php
@@ -0,0 +1,42 @@
+<?php
+  //error_reporting(E_ALL);
+  //ini_set("display_errors", 1);
+  //ini_set("allow_url_fopen", 1);
+//  date_default_timezone_set('Asia/Seoul');
+  //ini_set( 'date.timezone', 'Asia/Seoul' );
+
+  $url = "https://spreadsheets.google.com/feeds/list/1gzrxItSXDgTTnCaolsZXoAZggq4voNTc2Nbyz_J_Lf4/1/public/basic?alt=json-in-script";
+//  $url = "http://www.cdc.go.kr/linkCheck.es?mid=a21111050500";
+
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_URL, $url);
+
+  $result = curl_exec($ch);
+
+  curl_close($ch);
+
+  $result = str_replace("gdata.io.handleScriptLoaded(", "", $result);
+  $result = str_replace("$", "", $result);
+  $result = substr_replace($result, "", strlen($result)-1);
+  $result = substr_replace($result, "", strlen($result)-1);
+
+  $data = json_decode($result);
+
+  $entry = $data -> feed -> entry;
+  $content = $entry[0] -> content -> t;
+
+  $arrlength = count($entry);
+  $allString = "";
+
+  for($k = 0; $k < $arrlength; $k++){
+
+    $allString .= $entry[$k] -> title -> t;
+    $allString .= $entry[$k] -> content -> t;
+
+  }
+
+  echo $allString;
+
+?>
